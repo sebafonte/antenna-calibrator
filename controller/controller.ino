a150153063fbd45bd5cf8adf77b5e707db069fd1
sebafonte <sebafonte@gmail.com>
2017-05-13 08:04:12 -0100

    * Updated controller.ino, works fine.

--- controller/controller.ino
+++ controller/controller.ino
@@ -1,39 +1,18 @@
 char incomingValue[4];
 int index;
-int currentPosition;
+int currentAngle;
 int value=0;
-
+float degreesPerStep = 1.8;
 const int stepPinX = 3; 
 const int dirPinX = 5; 
 
-
-void setInitialPosition() {
-  
-}
-
-void setPosition(int angle) {
-  
-}
-
-int readCommand() {
-  if (Serial.available() > 0) {
-    byte incomingByte = Serial.read();
-    incomingValue[index++] = incomingByte;
-    if (incomingByte == 0) {
-      index = 0;
-      return atoi(incomingValue);
-    }
-  }
-  else
-    return -1;
-}
 
 void setup() {
   Serial.begin(9600);
   
   incomingValue[0] = 0;
   index = 0;
-  currentPosition = 0;
+  currentAngle = 0;
 
   pinMode(stepPinX, OUTPUT); 
   pinMode(dirPinX, OUTPUT);
@@ -43,22 +22,90 @@ void setup() {
   digitalWrite(stepPinX, LOW);
 }
 
-void loop() {
 
-  delay(1);
-  digitalWrite(LED_BUILTIN, LOW);
-  digitalWrite(stepPinX, HIGH);
-  delay(1);
-  digitalWrite(LED_BUILTIN, HIGH);
-  digitalWrite(stepPinX, LOW);
-  // Read serial command
-  
-  //digitalWrite(stepPinX, HIGH);
-  //digitalWrite(dirPinX, HIGH);
-  
-/*
-  if (angle >= 0) {
-    setPosition(angle);
+int readAngleCommand() {
+  if (Serial.available() > 0) {
+    char incomingByte = Serial.read();
+    //Serial.print("Readed char: ");
+    //Serial.println(incomingByte);
+   
+    if (!isDigit(incomingByte)) {
+      int value = -1;
+      
+      if (index > 0) {
+        incomingValue[index++] = '\n';
+        value = atoi(incomingValue);
+        
+        //Serial.print("Readed: ");
+        //Serial.println(value);
+      }
+      index = 0;
+      return value;
+    }
+    else
+    {
+      incomingValue[index++] = incomingByte;
+    }
   }
-*/
+  
+  return -1;
+}
+
+void setInitialPosition() {
+  currentAngle = 0;
+}
+
+int stepsForAngle(float angle) {
+  return angle / degreesPerStep;
+}
+
+void setRelativePosition(int angle) {
+  int steps = stepsForAngle(angle);
+
+  for (int i=0; i< steps; i++) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    digitalWrite(stepPinX, HIGH);
+    delay(1);
+    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(stepPinX, LOW);
+    delay(1);
+  }
+}
+
+void setAbsolutePosition(int angle) {
+  // Calculate angle
+  float newAngle = angle - currentAngle;
+
+  // Set direction
+  if (newAngle > 0)
+    digitalWrite(dirPinX, LOW);
+  else {
+    newAngle = - newAngle;
+    digitalWrite(dirPinX, HIGH);
+  }
+  
+  // Step
+  int steps = stepsForAngle(newAngle);
+
+  for (int i=0; i< steps; i++) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    digitalWrite(stepPinX, HIGH);
+    delay(1);
+    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(stepPinX, LOW);
+    delay(1);
+  }
+
+  currentAngle = (float) angle;
+}
+
+void loop() {
+  //setInitialPosition();
+  int angle = readAngleCommand();
+  
+  if (angle > -1) {
+    //setRelativePosition(angle);
+    setAbsolutePosition(angle);
+    delay(1000);
+  }
 }
--- motor-test/motor-test.ino
+++ motor-test/motor-test.ino
@@ -1,4 +1,4 @@
-char incomingValue[4];
+
 int index;
 int currentPosition;
 int value=0;
@@ -7,31 +7,10 @@ const int stepPinX = 3;
 const int dirPinX = 5; 
 
 
-void setInitialPosition() {
-  
-}
-
-void setPosition(int angle) {
-  
-}
-
-int readCommand() {
-  if (Serial.available() > 0) {
-    byte incomingByte = Serial.read();
-    incomingValue[index++] = incomingByte;
-    if (incomingByte == 0) {
-      index = 0;
-      return atoi(incomingValue);
-    }
-  }
-  else
-    return -1;
-}
 
 void setup() {
   Serial.begin(9600);
   
-  incomingValue[0] = 0;
   index = 0;
   currentPosition = 0;
 
@@ -44,21 +23,10 @@ void setup() {
 }
 
 void loop() {
-
   delay(1);
   digitalWrite(LED_BUILTIN, LOW);
   digitalWrite(stepPinX, HIGH);
   delay(1);
   digitalWrite(LED_BUILTIN, HIGH);
   digitalWrite(stepPinX, LOW);
-  // Read serial command
-  
-  //digitalWrite(stepPinX, HIGH);
-  //digitalWrite(dirPinX, HIGH);
-  
-/*
-  if (angle >= 0) {
-    setPosition(angle);
-  }
-*/
 }
